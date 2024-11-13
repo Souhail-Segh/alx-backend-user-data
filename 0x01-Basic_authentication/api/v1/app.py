da7e6b5722049ce9e0c3ee41f874bf97fae179b6
Souhail-Segh <souhail.seghds.ds@gmail.com>
2024-11-13 02:22:47 +0000

    No Literal in typing

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import Literal
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,7 +21,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple[str, Literal[401]]:
+def unauthorized(error) -> tuple:
     """ Unauthorized handler
     """
     return jsonify({"error": "unauthorized"}), 401
